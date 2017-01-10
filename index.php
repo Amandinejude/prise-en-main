2ad1e736edbb108e76459b1d9ea90fe08b15be02
Amandinejude <amandine.marchand.pro@gmail.com>
2017-01-10 11:33:27 +0100

    base de index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Mon site avec GIT</title>
+	</head>
+
+	<body>
+		<header>
+			<h1>Mon site avec GIT</h1>
+		</header>
+	</body>
+</html>
